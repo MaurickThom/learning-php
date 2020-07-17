870b08bf8db4df355fdaf50ca19be40bc7cc9df4
MaurickThom <thomtwd@gmail.com>
2020-07-17 15:12:57 -0500

    tipos de datos

--- 002/tipos_datos.php
+++ 002/tipos_datos.php
@@ -8,6 +8,28 @@
     if($boolean) {
         echo "verdadero";
     }else{
-        echo "Falso";
+        echo "Falso\n";
     }
+
+    # constantes
+    define('constante',10);
+    define('PI',3.1416);
+    const otraConstante = 15;
+    echo constante, "\n";
+    echo otraConstante, "\n";
+    echo PI,"\n";
+    echo gettype(otraConstante),"\n";
+    echo gettype($boolean),"\n";
+    echo gettype($string);
+    echo "\nhola ". "mundo";
+    $octal = 0755;
+    $hexadecimal = 0xC4E;
+    $binario = 0b1010;
+    echo gettype($hexadecimal);
+    echo gettype($octal);
+    echo gettype($binario);
+
+    # para saver que tipo son y ademas ver el dato que trae
+    var_dump($binario);
+    var_dump($string);
 ?>
