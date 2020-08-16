bd6e2e5fafde516179d58ec267ee8e0244220dda
MaurickThom <thomtwd@gmail.com>
2020-08-16 01:34:26 -0500

    shopping cart

---
+++ shopping-cart/models/bd/BD.php
@@ -0,0 +1,35 @@
+<?php 
+
+class DB{
+    private string $host;
+    private string $db;
+    private string $user;
+    private string $password;
+    private string $charset;
+
+    public function __construct(){
+        $this->host     = "localhost";
+        $this->db       = "dbShoppingCart";
+        $this->user     = "root";
+        $this->password = "mysql";
+        $this->charset  = 'utf8mb4';
+    }
+
+    //mysql -e "USE todolistdb; select*from todolist" --user=azure --password=6#vWHD_$ --port=49175 --bind-address=52.176.6.0
+
+    function connect():PDO{
+        try{
+            $connection = "mysql:host=".$this->host.";dbname=" . $this->db . ";charset=" . $this->charset;
+            $options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+            $pdo = new PDO($connection, $this->user, $this->password, $options);
+            // $pdo = new PDO($connection,$this->user,$this->password);
+            return $pdo;
+        }catch(PDOException $e){
+            print_r('Error connection: ' . $e->getMessage());
+        }   
+    }
+}
+?>
---
+++ shopping-cart/models/query/query.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE IF NOT EXISTS dbShoppingCart;
+
+USE dbShoppingCart;
+
+CREATE TABLE IF NOT EXISTS categories(
+    id_category INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    CONSTRAINT uq_name UNIQUE (name)
+);
+
+CREATE TABLE IF NOT EXISTS products(
+    id_product INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_category INT NOT NULL,
+    name_product VARCHAR(100) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL NOT NULL,
+    CONSTRAINT fk_categProduc FOREIGN KEY (id_category)
+    REFERENCES categories(id_category),
+    CHECK(quantity >= 0),
+    CHECK(price >= 0)
+);
+
+ALTER TABLE products ADD img VARCHAR(100) NOT NULL;
+
+
+INSERT INTO categories(name)  VALUES ("juguetes"),("libros");
+
+INSERT  INTO products(id_category,name_product,price,quantity,img) VALUES
+(1,"BATMAN MALO",500,10,"/public/img/juguete001.jpg"),
+(1,"CAPITAN AMERICA",260,5,"/public/img/juguete002.jpg"),
+(1,"THANOS",260,1,"/public/img/juguete003.jpg"),
+(1,"MISTERIO",340,100,"/public/img/juguete004.jpg"),
+(1,"SHAZAM",340,3,"/public/img/juguete005.jpg"),
+(1,"STICH",340,10,"/public/img/juguete006.jpg"),
+(2,"LIBRO A",340,100,"/public/img/libro01.jpg"),
+(2,"LIBRO B",260,1,"/public/img/libro02.jpg"),
+(2,"LIBRO C",340,3,"/public/img/libro03.jpg"),
+(2,"LIBRO D",340,10,"/public/img/libro04.jpg"),
+(2,"LIBRO E",340,10,"/public/img/libro05.jpg"),
+(2,"LIBRO F",340,10,"/public/img/libro06.jpg");
