a610a024145b178256c69f8e1e49c77b0f7ae48f
MaurickThom <thomtwd@gmail.com>
2020-08-25 22:42:13 -0500

    css and js production

--- page/resources/views/layout.blade.php
+++ page/resources/views/layout.blade.php
@@ -5,20 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel en minutos ... </title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     Menu
-    <nav>
-        <ul>
-            <li><a href="{{route('home')}}">Home</a></li>
-            <li><a href="{{route('about')}}">About</a></li>
-            <li><a href="{{route('blog.index')}}">Blog</a></li>
-            <li><a href="{{route('contacts')}}">Contacts</a></li>
+    <nav class="menu-container">
+        <ul class="menu">
+            <li class="menu__item"><a class="menu__link" href="{{route('about')}}">About</a></li>
+            <li class="menu__item"><a class="menu__link" href="{{route('blog.index')}}">Blog</a></li>
+            <li class="menu__item"><a class="menu__link" href="{{route('home')}}">Home</a></li>
+            <li class="menu__item"><a class="menu__link" href="{{route('contacts')}}">Contacts</a></li>
         </ul>
     </nav>
 
-    <div class="">
+    <div class="content">
         @yield('content')
     </div>
+    <script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
