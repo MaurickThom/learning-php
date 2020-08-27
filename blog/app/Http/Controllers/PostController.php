1b82b7737bbe3a30ca090dde4595ba7f150dd309
MaurickThom <thomtwd@gmail.com>
2020-08-27 01:53:11 -0500

    PostController file created

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     //
+    public function show(Post $post){
+        return view('post',compact('post'));
+    }
 }
